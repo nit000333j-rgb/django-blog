2e73ba9c04c941e1b88afc0769dec1ee887afb5f
nit000333j-rgb <nit000333j@gmail.com>
2026-05-14 14:26:45 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- myblog/settings.py
+++ myblog/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-blog-qgic.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
